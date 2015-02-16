9a3700b60aa571ac77f4d41436989441a2fa2773
Soulou <leo@unbekandt.eu>
2015-02-16 22:53:04 +0100

    Quit without error on Ctrl^D

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 	"strings"
@@ -38,6 +39,10 @@ func main() {
 	for {
 		text, err := term.ReadLine()
 		if err != nil {
+			if err == io.EOF {
+				// Quit without error on Ctrl^D
+				break
+			}
 			panic(err)
 		}
 
