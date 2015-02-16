2ec410b2a7d938c45980b2d63f24dff1a12febf2
Daniel Theophanes <kardianos@gmail.com>
2015-02-16 14:20:22 -0800

    Support windows. go fmt main.go.

--- main.go
+++ main.go
@@ -1,64 +1,71 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strings"
-    "strconv"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
 )
 
 import (
-    "github.com/alfredxing/calc/compute"
+	"github.com/alfredxing/calc/compute"
 )
 
 import (
-    "golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func main() {
-    if len(os.Args) > 1 {
-        input := strings.Replace(strings.Join(os.Args[1:], ""), " ", "", -1)
-        res, err := compute.Evaluate(input)
-        if err != nil {
-            fmt.Println("Error: " + err.Error())
-            return
-        }
-        fmt.Printf("%s\n", strconv.FormatFloat(res, 'G', -1, 64))
-        return
-    }
+	if len(os.Args) > 1 {
+		input := strings.Replace(strings.Join(os.Args[1:], ""), " ", "", -1)
+		res, err := compute.Evaluate(input)
+		if err != nil {
+			fmt.Println("Error: " + err.Error())
+			return
+		}
+		fmt.Printf("%s\n", strconv.FormatFloat(res, 'G', -1, 64))
+		return
+	}
 
-    oldState, err := terminal.MakeRaw(0)
-    if err != nil {
-        panic(err)
-    }
-    defer terminal.Restore(0, oldState)
+	if runtime.GOOS != "windows" {
+		oldState, err := terminal.MakeRaw(0)
+		if err != nil {
+			panic(err)
+		}
+		defer terminal.Restore(0, oldState)
+	}
 
-    term := terminal.NewTerminal(os.Stdin, "> ")
-    term.AutoCompleteCallback = handleKey
-    for {
-        text, err := term.ReadLine()
-        if err != nil {
-            panic(err)
-        }
+	term := terminal.NewTerminal(os.Stdin, "> ")
+	term.AutoCompleteCallback = handleKey
+	for {
+		text, err := term.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
 
-        text = strings.Replace(text, " ", "", -1)
-        if text == "exit" || text == "quit" {
-            break
-        }
+		text = strings.Replace(text, " ", "", -1)
+		if text == "exit" || text == "quit" {
+			break
+		}
 
-        res, err := compute.Evaluate(text)
-        if err != nil {
-            fmt.Println("Error: " + err.Error())
-            continue
-        }
-        fmt.Printf("%s\n", strconv.FormatFloat(res, 'G', -1, 64))
-    }
+		res, err := compute.Evaluate(text)
+		if err != nil {
+			fmt.Println("Error: " + err.Error())
+			continue
+		}
+		fmt.Printf("%s\n", strconv.FormatFloat(res, 'G', -1, 64))
+	}
 }
 
 func handleKey(line string, pos int, key rune) (newLine string, newPos int, ok bool) {
-    if key == '\x03' {
-        fmt.Println()
-        os.Exit(0)
-    }
-    return "", 0, false
+	if key == '\x03' {
+		fmt.Println()
+		os.Exit(0)
+	}
+	return "", 0, false
 }
